18bc6c47131450ff158295b79e1328d4e796023e
Shaun Luttin <admin@shaunluttin.com>
2016-11-10 09:38:57 -0800

    Print deck with royalty

---
+++ Concepts.md
@@ -0,0 +1,5 @@
+# Concepts
+
+# Discriminating Union
+
+* 
---
+++ Program.fs
@@ -0,0 +1,31 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+// discriminating union
+type Value = 
+    | Ace
+    | King
+    | Queen
+    | Jack
+    | Rank of int
+    
+// discriminating union
+type Suit =
+    | Clubs
+    | Hearts
+    | Spades
+    | Diamonds
+
+type Card = Card of Value*Suit
+let PrintDeck = 
+    [
+        for s in [Clubs; Hearts; Spades; Diamonds] do 
+            for v in [Ace; King; Queen; Jack] do 
+                yield (v, s)
+    ]
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" PrintDeck
+    0 // return an integer exit code
